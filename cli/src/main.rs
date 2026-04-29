1e4513cf0c2fd55d41ccadd61c64f7b94a4b050f
jad <jad.bathore@gmail.com>
2026-04-29 19:29:50 +0200

    main

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -4,8 +4,9 @@ use clap::Parser;
 use commun_utils_handler::{errors::GlobalError, fs_strategies::FileReader};
 use derive_utils::IterableStringifyEnum;
 use commun_utils_handler::IterableStringifyEnum;
-use tokio::{net::TcpListener};
-
+use tokio::{io::AsyncWriteExt, net::TcpStream};
+use tokio_tungstenite::{accept_async, connect_async, tungstenite::Message};
+use futures::{SinkExt, StreamExt};
 
 fn parse_file_reader(s:&str)-> Result<FileReader, String> 
 {
@@ -21,9 +22,7 @@ enum Command {
 
 #[derive(Parser,Debug)]
 struct Cli{
-
     pattern: Command,
-
     #[arg(value_parser = parse_file_reader)]
     path:Option<FileReader>,
 }
@@ -36,14 +35,22 @@ async fn main()->Result<(), Box<dyn Error>>
     match args.pattern {
         Command::AddFile => {
             if let Some(file) = args.path {
-                let listener = TcpListener::bind("localhost:8080").await.unwrap();
+                let (stream,_) = connect_async("ws://localhost:8080").await?;
+
+                // let stream = TcpStream::connect("localhost:8080").await?;
+                // let mut ws = accept_async(stream).await?;
+                let (mut write,read) = stream.split();
+                let _ = write.send(Message::Text(file.to_string_lossy().to_string())).await;
+                let mut buffers = Vec::new(); 
+                file.flush_data(&mut buffers)?;
+                for buffer in buffers {
+                    let _ = write.send(Message::Binary(buffer.to_vec())).await;
+                } 
+                // let listener = TcpListener::bind("localhost:8080").await.unwrap();
                 // let mut buffers = Vec::new();
             
                 // stream.write_all(file.to_string_lossy().as_bytes())?;
-                // file.flush_data(&mut buffers)?;
-                // for buffer in buffers {
-                //     stream.write_all(&buffer)?;
-                // } 
+                
                 // stream.flush()?;  
                 // dbg!()
             } else {
