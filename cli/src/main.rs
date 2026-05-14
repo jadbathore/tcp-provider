6e2ea4d0b2e8ebee4e2b36c9a5cc9405df1e5239
jad <jad.bathore@gmail.com>
2026-05-14 18:24:09 +0200

    sending flag

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,10 +1,10 @@
-use std::{error::Error, io::{Read, Write}, path::Path,pin::Pin};
+use std::{error::Error, io::{Read, Write}, path::Path,pin::Pin, str::FromStr};
 
 use clap::Parser;
 use commun_utils_handler::{ errors::GlobalError, fs_strategies::FileReader};
 use derive_utils::IterableStringifyEnum;
 use tokio::{io::AsyncWriteExt, net::TcpStream};
-use tokio_tungstenite::{MaybeTlsStream, WebSocketStream, accept_async, connect_async, tungstenite::{Message, client::connect_with_config, handshake::server::Callback, protocol::WebSocketConfig}};
+use tokio_tungstenite::{WebSocketStream, accept_async, connect_async, tungstenite::{Message, WebSocket, client::{IntoClientRequest, connect_with_config}, connect, handshake::server::Callback, protocol::{WebSocketConfig, frame::Frame}, stream::MaybeTlsStream}};
 use futures::{SinkExt, StreamExt, stream::{self, SplitSink, SplitStream}};
 
 fn parse_file_reader(s:&str)-> Result<FileReader, String> 
@@ -21,6 +21,12 @@ enum Command {
     Type
 }
 
+#[derive(IterableStringifyEnum,Debug)]
+enum Flag {
+    File,
+    Directory
+}
+
 
 #[derive(Parser,Debug)]
 struct Cli{
@@ -29,27 +35,69 @@ struct Cli{
     path:Option<FileReader>,
 }
 
-type WriterSender = SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>;
-type ReaderSender = SplitStream<WebSocketStream<tokio_tungstenite::MaybeTlsStream<tokio::net::TcpStream>>>;
+type WriterSender = WebSocket<MaybeTlsStream<std::net::TcpStream>>;
+// type ReaderSender = SplitStream<WebSocketStream<tokio_tungstenite::MaybeTlsStream<tokio::net::TcpStream>>>;
 
-async fn connect_with_protocol<F>(command_line_args:Cli,callback:F)->Result<(),Box<dyn Error>> 
+fn connect_with_protocol<F>(command_line_args:Cli,callback:F)->Result<(),Box<dyn Error>> 
     where   
-        F: for<'a> FnOnce(&'a mut WriterSender,&'a mut ReaderSender,Option<FileReader>)->BoxFuture<'a,Result<(),Box<dyn Error>>>
+        F: FnOnce(&mut WriterSender,Option<FileReader>)->Result<(),Box<dyn Error>>
 {
-    let (stream,_) = connect_async("ws://localhost:8080").await?;
-    let (mut write,mut read) = stream.split();
-    let _ = write.send(Message::Text(command_line_args.pattern.into())).await;
-    callback(&mut write,&mut read,command_line_args.path).await?;
+    let mut request = "ws://localhost:8080".into_client_request()?;
+    let pattern_string :String = command_line_args.pattern.into();
+    request.headers_mut().append("sec-websocket-protocol",pattern_string.parse()?);
+    let (mut stream,_) = connect(request)?;
+
+    // stream.write(Message::Text(command_line_args.pattern.into()))?;     
+    // let (mut write,mut read) = stream.split();
+
+    // let _ = write.send(Message::Text(command_line_args.pattern.into())).await;
+    callback(&mut stream,command_line_args.path)?;
     Ok(())
 }
 
+// type WriterSenderAsync = SplitSink<WebSocketStream<tokio_tungstenite::MaybeTlsStream<TcpStream>>, Message>;
+// type ReaderSenderAsync = SplitStream<WebSocketStream<tokio_tungstenite::MaybeTlsStream<tokio::net::TcpStream>>>;
 
-async fn handle_add_file(write:&mut WriterSender,read:&mut ReaderSender,file_reader:Option<FileReader>)->Result<(),Box<dyn Error>>
+// async fn connect_async_with_protocol<F>(command_line_args:Cli,callback:F)->Result<(),Box<dyn Error>> 
+//     where   
+//         F: for<'a> FnOnce(&'a mut WriterSenderAsync,&'a mut ReaderSenderAsync,Option<FileReader>)->BoxFuture<'a,Result<(),Box<dyn Error>>>
+// {
+//     let mut request = "ws://localhost:8080".into_client_request()?;
+//     let pattern_string :String = command_line_args.pattern.into();
+//     request.headers_mut().append("sec-websocket-protocol",pattern_string.parse()?);
+//     let (stream,_) = connect_async(request).await?;
+//     let (mut write,mut read) = stream.split();
+//     // let _ = write.send(Message::Text(command_line_args.pattern.into())).await;
+//     callback(&mut write,&mut read,command_line_args.path).await?;
+//     Ok(())
+// }
+
+
+
+fn handle_add_file(write:&mut WriterSender,file_reader:Option<FileReader>)->Result<(),Box<dyn Error>>
 {
     if let Some(file) = file_reader {
-        let _ = write.send(Message::Text(file.to_string_lossy().to_string())).await;
+        let _ = write.send(Message::Text(file.to_string_lossy().to_string()));
         let mut buffers = Vec::new();
         file.flush_data(&mut buffers)?;
+
+        let flag_use:Flag = match buffers.len() {
+            x if x <= 1 =>  Flag::File,
+            _ => Flag::Directory
+        };
+       
+        let _ = write.send(Message::Text(flag_use.into()));
+        
+        let size:usize = file.size()?.try_into()?;
+        // let a:usize = size.to_string().try_into;
+
+        // let byte_num = size.to_be_bytes().to_vec();
+        let _ = write.send(Message::Text(size.to_string()));
+        for buffer in buffers.iter(){
+            let _ = write.send(Message::Binary(buffer.to_vec()));
+        }
+        println!("data sended");
+
         Ok(())
     } else {
         return Err(Box::new(GlobalError::ParseError("missing file".to_string())));
@@ -64,10 +112,12 @@ async fn main()->Result<(), Box<dyn Error>>
     let args = Cli::parse();
     match args.pattern {
         Command::AddFile => {
-            connect_with_protocol(args,|write,read,path|
-                {
-                    Box::pin(handle_add_file(write, read,path))
-                }).await?;
+
+            connect_with_protocol(args, handle_add_file)?;
+            // connect_with_protocol(args,|write,read,path|
+            //     {
+            //         Box::pin(handle_add_file(write, read,path))
+            //     }).await?;
 
             // if let Some(_) = args.path {
             //     // let config = WebSocketConfig::default().
